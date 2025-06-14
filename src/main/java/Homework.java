d93dc90fafdf42fc6eb8e0796370b58208d71587
misha <mikerube07@gmail.com>
2025-06-14 23:23:13 +0300

    homework1

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -1,5 +1,20 @@
 public class Homework {
     public static void main(String[] args) {
         System.out.println("Hello");
+        check(35);
+    }
+
+    public static void check(int n) {
+        boolean mod3 = n % 3 == 0;
+        boolean mod5 = n % 5 == 0;
+        if (mod3 && mod5) {
+            System.out.println("FizzBuzz");
+        } else if (mod3) {
+            System.out.println("Fizz");
+        } else if (mod5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(n);
+        }
     }
 }
